769355686ec48be3ee92bb4c74345e43913bac45
stella <stellamankah@yahoo.com>
2021-10-23 22:54:07 -0400

    New change

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Stella <code>src/App.tsx</code> and save to reload.
+          Stella Snyk Demo <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
